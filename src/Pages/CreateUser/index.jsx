d92f0185e01172ec46944a3a06f3dce2732fda2f
Muhammad Sualeh <sualeh.naeem@gmail.com>
2023-08-22 19:07:16 +0500

    Create form page wokring with database

--- src/Pages/CreateUser/index.jsx
+++ src/Pages/CreateUser/index.jsx
@@ -1,24 +1,83 @@
 import { useState } from "react";
-import axios from "axios";
+// import axios from "axios";
 import "./styles.css";
 const CreateUser = () => {
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
-  const api = () => {
-    const response = fetch("localhost:3000/admin/createUser");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [addressId, setAddressId] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const createUserHandler = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/admin/createUser", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+          addressId,
+        }),
+      });
+      if (response) {
+        setIsSubmitted(true);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
     <div className="createUser">
       <h1>Create User</h1>
 
-      <form action="">
-        <input type="text" placeholder="Enter Your Full Name" />
-        <input type="email" placeholder="Enter Your E-mail" />
-        <input type="password" placeholder="Enter Your Password" />
-        <input type="number" placeholder="Enter Your Address Id" />
-        <button>Create Account</button>
+      <form onSubmit={createUserHandler} action="">
+        <input
+          type="text"
+          placeholder="Enter Your Full Name"
+          id="name"
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Enter Your E-mail"
+          id="email"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Enter Your Password"
+          id="password"
+          value={password}
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+          required
+        />
+        <input
+          type="number"
+          placeholder="Enter Your Address Id"
+          id="addressId"
+          value={addressId}
+          onChange={(event) => {
+            setAddressId(event.target.value);
+          }}
+          required
+        />
+        {isSubmitted && (
+          <p className="submitted">Your form has been submitted</p>
+        )}
+        <button type="Submit">Create Account</button>
       </form>
     </div>
   );
