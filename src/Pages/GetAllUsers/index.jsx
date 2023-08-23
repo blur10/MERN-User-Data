9b90749dc269a4d0e593f3a11b4ab6c35220f5ca
Muhammad Sualeh <sualeh.naeem@gmail.com>
2023-08-23 17:29:52 +0500

    Finished getAllUsers module

--- src/Pages/GetAllUsers/index.jsx
+++ src/Pages/GetAllUsers/index.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
+import { useEffect } from "react";
 import "./styles.css";
 const GetAllUsers = () => {
+  // const [id, setId] = useState();
+  // const [name, setName] = useState();
+  // const [email, setEmail] = useState();
+  // const data = {
+  //   id,
+  //   name,
+  //   email,
+  // };
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/admin/getAllUsers") // Replace with your actual API endpoint
+      .then((response) => response.json())
+      .then((data) => {
+        setUserData(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
   return (
-    <div>
+    <div className="getAllUsers">
       <h1>Users</h1>
+      <table>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+        {userData.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+          </tr>
+        ))}
+        s{" "}
+      </table>
     </div>
   );
 };
